debb5b68b98390d51ef8064731957e8f0389d198
FBL7261 <felipe.burgos1901@alumnos.ubiobio.cl>
2023-10-26 00:35:41 -0300

    Se actualiza controlador

--- backend/src/controller/revision.controller.js
+++ backend/src/controller/revision.controller.js
@@ -2,22 +2,50 @@
 
 const { respondSuccess, respondError } = require("../utils/resHandler.js");
 const RevisionService = require("../services/revision.service.js");
-//const { revisionBodySchema, revisionIdSchema } = require("../schema/revision.schema.js");
+const { revisionBodySchema, revisionIdSchema } = require("../schema/revision.schema.js");
 const { handleError } = require("../utils/errorHandler.js");
 
-async function getRevision(req, res) {
+async function createRevision(req, res) {
   try {
-    const [revisiones, errorRevisiones] = await RevisionService.getRevision();
-    if (errorUsuarios) return respondError(req, res, 404, errorUsuarios);
-
-    usuarios.length === 0
-      ? respondSuccess(req, res, 204)
-      : respondSuccess(req, res, 200, usuarios);
+    const { body } = req;
+    const { error: bodyError } = revisionBodySchema.validate(body);
+    if (bodyError) {
+      return respondError(req, res, 400, bodyError.message);
+    }
+    const [newRevision, revisionError] = await RevisionService.createRevision(body);
+    if (revisionError) {
+      return respondError(req, res, 400, revisionError);
+    }
+    if (!newRevision) {
+      return respondError(req, res, 400, "No se creo la revivion");
+    }
+    respondSuccess(req, res, 201, newRevision);
   } catch (error) {
-    handleError(error, "revision.controller -> getRevision");
+    handleError(error, "revisionController -> createRevision");
     respondError(req, res, 400, error.message);
   }
 }
+
+async function getRevision(req, res) {
+  try {
+    const { params } = req;
+    const { error: paramsError } = revisionIdSchema.validate(params);
+    if (paramsError) {
+      return respondError(req, res, 400, paramsError.message);
+    }
+
+    const [revision, revisionError] = await RevisionService.getRevision(params.id);
+    // Verificacion de errores
+    if (revisionError) {
+      return respondError(req, res, 404, revisionError);
+    }
+    respondSuccess(req, res, 200, revision);
+  } catch (error) {
+    handleError(error, "revision.controller -> getRevision");
+    respondError(req, res, 500, "No se pudo obtener la revision previa");
+  }
+}
+
 async function deleteRevision(req, res) {
     try {
       const { params } = req;
@@ -41,6 +69,7 @@ async function deleteRevision(req, res) {
   }
 
 module.exports = {
+    createRevision,
     getRevision,
     deleteRevision,
   };
