d1876135475614655c4ac821bbbd77d0bc31a324
FBL7261 <felipe.burgos1901@alumnos.ubiobio.cl>
2023-10-26 11:06:12 -0300

    agrega controlador compararDatosEnviados V1

--- backend/src/controller/revision.controller.js
+++ backend/src/controller/revision.controller.js
@@ -4,6 +4,7 @@ const { respondSuccess, respondError } = require("../utils/resHandler.js");
 const RevisionService = require("../services/revision.service.js");
 const { revisionBodySchema, revisionIdSchema } = require("../schema/revision.schema.js");
 const { handleError } = require("../utils/errorHandler.js");
+const { userSchema } = require("../schema/user.schema.js");
 
 async function createRevision(req, res) {
   try {
@@ -25,6 +26,8 @@ async function createRevision(req, res) {
     respondError(req, res, 400, error.message);
   }
 }
+
+
 
 async function getRevision(req, res) {
   try {
@@ -54,13 +57,7 @@ async function deleteRevision(req, res) {
   
       const revision = await RevisionService.deleteRevision(params.id);
       !revision
-        ? respondError(
-            req,
-            res,
-            404,
-            "No se encontro el usuario solicitado",
-            "Verifique el id ingresado",
-          )
+        ? respondError(req,res,404,"No se encontro la revision", "Verifique el id",)
         : respondSuccess(req, res, 200, revision);
     } catch (error) {
       handleError(error, "revision.controller -> deleteRevision");
@@ -68,9 +65,48 @@ async function deleteRevision(req, res) {
     }
   }
 
+async function compararDatosEnviados(req, res) {
+  try {
+    const { body } = req;
+    const { error: bodyError } = userSchema.validate(body); 
+
+    if (bodyError) {
+      return respondError(req, res, 400, bodyError.message);
+    }
+
+    const usuarioActual = req.user; 
+
+    const datosRepetidos = compararDatos(usuarioActual, body);
+
+    if (datosRepetidos.length > 0) {
+      return respondError(req, res, 400, "Se encontraron datos repetidos", datosRepetidos);
+    }
+
+    respondSuccess(req, res, 200, "No se encontraron datos repetidos");
+  } catch (error) {
+    handleError(error, "compararDatosDePostulantes");
+    respondError(req, res, 500, "Error al comparar datos de postulantes");
+  }
+}
+
+function compararDatos(usuarioActual, datosPostulante) {
+  const datosRepetidos = [];
+
+  if (usuarioActual.nombre === datosPostulante.nombre) {
+    datosRepetidos.push("nombre");
+  }
+
+  if (usuarioActual.rut === datosPostulante.rut) {
+    datosRepetidos.push("rut");
+
+  return datosRepetidos;
+  }
+}
+
 module.exports = {
     createRevision,
     getRevision,
     deleteRevision,
+    compararDatosEnviados,
   };
   
