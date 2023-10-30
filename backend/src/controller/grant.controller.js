e53f9ce839976546789ae5ffe8613356e2759eee
lmxti <mvtias.smm@gmail.com>
2023-10-29 22:46:39 -0300

    UPDATE 10.29 COMMENTS CONTROLLER-SERVICE GRANT

--- backend/src/controller/grant.controller.js
+++ backend/src/controller/grant.controller.js
@@ -1,22 +1,52 @@
-const { respondSuccess, respondError } = require("../utils/resHandler.js");
-const GrantService = require('../services/grant.service.js');
-const { handleError } = require("../utils/errorHandler.js");
-const { grantBodySchema } = require("../schema/grant.schema.js");
-
+// <----------------- MODELOS IMPORTADOS ----------------------->
+// Modelo de "person" (usuarios)
 const Person = require('../models/person.model.js');
+// <---------------- SCHEMAS DE VALIDACION --------------------->
+// Schema de validacion de datos de "Grant" (beca)
+const { grantBodySchema } = require("../schema/grant.schema.js");
+// <---------------- SERVICIOS IMPORTADOS ---------------------->
+// Servicios de "Grant" (beca)
+const GrantService = require('../services/grant.service.js');
+// Servicio de envio de correos electronicos
 const NodeMailer = require('../services/nodemailer.service.js');
+// <--------------------- UTILIDADES --------------------------->
+// Respuestas estandarizadas
+const { respondSuccess, respondError } = require("../utils/resHandler.js");
+// Manejador de errores
+const { handleError } = require("../utils/errorHandler.js");
 
-// <----------------------------- Obtener todas las becas ----------------------------->
+
+// <-------------------------------------------------------------------->
 /**
- * Obtiene todas las becas de la base de datos
- * @param {Object} req - Objeto de solicitud
- * @param {Object} res - Objeto de respuesta
+ * @name createGrant
+ * @description Crea una nueva beca
+ * @param {Object} req - Objeto de solicitud con los datos de la beca a crear
+ * @param {Object} res - Objeto de respuesta con la beca creada
  */
-async function getGrants(req, res) {
+async function createGrant(req, res) {
     try {
-        // Obtencion de todas las becas en la BD
-        const [grants, errorGrants] = await GrantService.getGrants();
-        // Verificacion de errores
+        // Desestructuracion de datos de la solicitud
+        const { body } = req;
+        // Validacion de campos de body
+        const { error: bodyError } = grantBodySchema.validate(body);
+        // Si existen errores en los campos de body
+        if (bodyError){
+            return respondError(req, res, 400, bodyError.message);
+        }
+        // Servicio de creacion de beca
+        const [newGrant, grantError] = await GrantService.createGrant(body);
+        // Si hay un error al crear la beca
+        if (grantError) return respondError(req, res, 400, grantError);
+        // Si no hay datos de creacion (false)
+        if (!newGrant) return respondError(req, res, 400, "No se pudo crear la beca");
+        // Respuesta exitosa
+        respondSuccess(req, res, 201, {
+            message: "Beca creada con exito",
+            data: newGrant,
+        });
+
+        // Obtencion de becas en la base de datos
+        const [allGrants, errorGrants] = await GrantService.getGrants();
         if (errorGrants) {
             return respondError(
                 res,
@@ -25,24 +55,95 @@ async function getGrants(req, res) {
                 errorGrants
             );
         }
-        grants.length === 0
-            ? respondSuccess(req, res, 200, "No hay becas registradas")
-            : respondSuccess(req, res, 200, {
+        // Busqueda de personas (usuarios) en la base de datos
+        Person.find({})
+        .then((persons) => {
+            if (!persons || persons.length === 0) {
+            return respondError(req, res, 404, "No se encontraron usuarios");
+            }
+            // Envio de correo a cada usuario con un listado de las becas disponibles
+            persons.forEach((user) => {
+                // Datos de usuario a enviar correo
+                let nameUser = user["name"];
+                let emailUser = user["email"];
+                // Mensaje para enviar a usuario
+                let message = `Hola ${nameUser}, se ha creado una nueva beca ${body.name}. Aquí están todas las becas disponibles:\n`;
+                // Recorrido de becas para agregarlas al mensaje
+                allGrants.forEach((grant) => {
+                    message += `- ${grant.name} - Monto: ${grant.amount}\n`;
+                });
+                // Envio de correo a usuario
+                NodeMailer.enviarEmail(emailUser, "Nueva beca creada", message);
+            });
+        })
+        .catch((error) => {
+            console.error("Error al obtener los usuarios:", error);
+            return respondError(req, res, 500, "Error al obtener los usuarios");
+        });
+    } catch(error) {
+        handleError(error, "grant.controller -> createGrant");
+        respondError(req, res, 400, error.message);
+    }
+}
+
+// <-------------------------------------------------------------------->
+/**
+ * @name getGrants
+ * @description Obtiene todas las becas registradas en la base de datos
+ * @param {Object} req - Objeto de solicitud
+ * @param {Object} res - Objeto de respuesta
+ */
+async function getGrants(req, res) {
+    try {
+        const [grants, errorGrants] = await GrantService.getGrants();
+        // Si hay un error al obtener las becas
+        if (errorGrants) {
+            return respondError(
+                res,
+                500,
+                "Error al obtener las becas",
+                errorGrants
+            );
+        }
+        // Si no existen becas en la base de datos
+        if(grants.length === 0){
+            return respondSuccess(req, res, 200, "No hay becas registradas");
+        }
+        // Si existen becas en la base de datos 
+        else {
+            respondSuccess(req, res, 200, {
                 message: "Becas obtenidas con exito",
                 data: grants,
             });
+        }
     } catch(error) {
         handleError(error, "grant.controller -> getGrants");
         respondError(req, res, 400, error.message);
     }
 }
 
+// <-------------------------------------------------------------------->
+/**
+ * @name getGrantById
+ * @description Obtiene una beca a traves de un id
+ * @param {Object} req - Objeto de solicitud con "id" de la beca a obtener
+ * @param {Object} res - Objeto de respuesta con la beca solicitada
+ */
 async function getGrantById(req, res) {
     try {
+        // Desestructuracion de datos (id) de la solicitud
         const { id } = req.params;
+        // Servicio de obtencion de beca por id
         const [grant, errorGrant] = await GrantService.getGrantById(id);
-        if (errorGrant) return respondError(req, res, 400, errorGrant);
-        if (!grant) return respondError(req, res, 400, "No se encontró la beca");
+        // Si hay un error al obtener la beca
+        if (errorGrant){
+            return respondError(req, res, 400, errorGrant);
+        }
+        // Si no existe la beca
+        if (!grant){
+            return respondError(req, res, 400, "No se encontró la beca");
+        }
+        // Respuesta exitosa
         respondSuccess(req, res, 200, {
             message: "Beca obtenida con exito",
             data: grant,
@@ -53,73 +154,28 @@ async function getGrantById(req, res) {
     }
 }
 
-async function createGrant(req, res) {
-    try {
-        const { body } = req;
-        // Validacion de campos de body
-        const { error: bodyError } = grantBodySchema.validate(body);
-        if (bodyError) return respondError(req, res, 400, bodyError.message);
+// <-------------------------------------------------------------------->
 
-        const [newGrant, grantError] = await GrantService.createGrant(body);
-        // Error de createGrant
-        if (grantError) return respondError(req, res, 400, grantError);
-        // Error si no hay datos (false)
-        if (!newGrant) return respondError(req, res, 400, "No se pudo crear la beca");
-        // Respuesta exitosa
-        respondSuccess(req, res, 201, {
-            message: "Beca creada con exito",
-            data: newGrant,
-        });
-
-        const [allGrants, errorGrants] = await GrantService.getGrants();
-        if (errorGrants) {
-            return respondError(
-                res,
-                500,
-                "Error al obtener las becas",
-                errorGrants
-            );
-        }
-
-        // Notificacion a todas las personas del sistema sobre la nueva beca creada + las becas existentes
-        Person.find({})
-        .then((persons) => {
-            if (!persons || persons.length === 0) {
-            return respondError(req, res, 404, "No se encontraron usuarios");
-            }
-            persons.forEach((user) => {
-                let nameUser = user["name"];
-                let emailUser = user["email"];
-                let message = `Hola ${nameUser}, se ha creado una nueva beca ${body.name}. Aquí están todas las becas disponibles:\n`;
-
-                allGrants.forEach((grant) => {
-                    message += `- ${grant.name} - Monto: ${grant.amount}\n`;
-                });
-                NodeMailer.enviarEmail(emailUser, "Nueva beca creada", message);
-            });
-        })
-        .catch((error) => {
-            console.error("Error al obtener los usuarios:", error);
-            return respondError(req, res, 500, "Error al obtener los usuarios");
-        });
-
-
-    } catch(error) {
-        handleError(error, "grant.controller -> createGrant");
-        respondError(req, res, 400, error.message);
-    }
-}
-
+/**
+ * @name updateGrantById
+ * @description Actualiza una beca a traves de un id
+ * @param {Object} req - Objeto de solicitud con los datos de la beca a actualizar
+ * @param {Object} res - Objeto de respuesta con la beca actualizada
+ * @returns 
+ */
 async function updateGrantById(req, res) {
     try {
+        // Desestructuracion de datos (id) de la solicitud
         const { id } = req.params;
+        // Desestructuracion de datos de la solicitud
         const { body } = req;
-        // Validacion de campos de body
+        // Validacion de campos de datos de body
         const { error: bodyError } = grantBodySchema.validate(body);
+        // Si existen errores en los campos de body
         if (bodyError) return respondError(req, res, 400, bodyError.message);
-
+        // Servicio de actualizacion de beca
         const [grantUpdated, grantError] = await GrantService.updateGrant(id, body);
-        // Error de updateGrant
+        // Si hay un error al actualizar la beca
         if (grantError) return respondError(req, res, 400, grantError);
         // Error si no hay datos (false)
         if (!grantUpdated) return respondError(req, res, 400, "No se pudo actualizar la beca");
@@ -134,15 +190,24 @@ async function updateGrantById(req, res) {
     }
 }
 
+// <-------------------------------------------------------------------->
+
+/**
+ * @name deleteGrantById
+ * @description Elimina una beca a traves de un id
+ * @param {Object} req - Objeto de solicitud con el "id" de la beca a eliminar
+ * @param {Object} res - Objeto de respuesta con la beca eliminada 
+ */
 async function deleteGrantById(req, res){
     try {
+        // Desestructuracion de datos (id) de la solicitud
         const { id } = req.params;
-        // Validar id en el schema de grant
-        // const {error: paramsError} = grantIdSchema.validate(id);
-
+        // Servicio de eliminacion de beca
         const grant = await GrantService.deleteGrantById(id);
         !grant
+        // Si la beca no existe o no se encontro
             ? respondError(req, res, 400, "No se encontró la beca", "Verifique el 'id' ingresado")
+        // Si la beca existe y se elimino con exito 
             : respondSuccess(req, res, 200, {
                 message: "Beca eliminada con exito",
                 data: grant,
@@ -154,14 +219,25 @@ async function deleteGrantById(req, res){
     
 }
 
+// <-------------------------------------------------------------------->
+
+/**
+ * @name desactivateGrantById
+ * @description Desactiva una beca a traves de un id
+ * @param {Object} req - Objeto de solicitud con el "id" de la beca a desactivar 
+ * @param {Object} res - Objeto de respuesta con la beca desactivada
+ */
 async function desactivateGrantById(req, res){
     try {
+        // Desestructuracion de datos (id) de la solicitud
         const { id } = req.params;
-        // Validar id en el schema de grant
-        // const {error: paramsError} = grantIdSchema.validate(id);
+        // Servicio de desactivacion de beca
         const grant = await GrantService.desactivateGrantById(id);
+
         !grant
+        // Si la beca no existe o no se encontro
             ? respondError(req, res, 400, "No se encontró la beca", "Verifique el 'id' ingresado")
+        // Si la beca existe y se desactivo con exito
             : respondSuccess(req, res, 200, {
                 message: "Beca desactivada con exito",
                 data: grant,
--- backend/src/services/grant.service.js
+++ backend/src/services/grant.service.js
@@ -2,8 +2,10 @@ const Grant = require('../models/grant.model');
 const { handleError } = require("../utils/errorHandler");
 const cron = require('node-cron');
 
+// <-------------------------------------------------------------------->
 /**
- * Obtiene todos las becas de la base de datos
+ * @name getGrants
+ * @description Obtiene todos las becas de la base de datos
  * @returns {Promise} Promesa con el objeto de las becas de la base de datos
  */
 async function getGrants() {
@@ -18,23 +20,44 @@ async function getGrants() {
     }
 }
 
+// <-------------------------------------------------------------------->
+/**
+ * @name getGrantById
+ * @description Servicio que se encarga deBusca una beca por su id
+ * @param {String} id Id de la beca a buscar
+ * @returns {Promise} Promesa con el objeto de la beca de la base de datos
+ */
 async function getGrantById(id) {
     try {
+        // Busqueda de beca por id en la base de datos
         const grant = await Grant.findById({_id : id}).exec();
+        // Verificacion de existencia de la beca
         if (!grant) {
             return [null, "No se encontró la beca"];
         }
+        // Retorno de la beca encontrada
         return [grant, null];
     } catch (error) {
         handleError(error, "grant.service -> getGrantById");
     }
 }
 
+// <-------------------------------------------------------------------->
+/**
+ * @name createGrant
+ * @description Servicio que se encarga de crear una beca
+ * @param {Object} grant - Objeto con los datos de la beca a crear 
+ * @returns {Promise} Promesa con el objeto de la beca creada
+ */
 async function createGrant(grant){
     try{
+        // Desestructuracion de los datos de la beca a crear
         const { name, documents, amount, requirements } = grant;
+        // Verificacion de existencia de la beca en la base de datos
         const grantFound = await Grant.findOne({ name: name });
+        // Retorno de error si la beca ya existe
         if (grantFound) return [null, `No se pudo crear la beca, ya existe una con el nombre '${name}'`];
+
 
         const newGrant = new Grant({
             name,
@@ -49,6 +72,14 @@ async function createGrant(grant){
     }
 }
 
+// <-------------------------------------------------------------------->
+/**
+ * @name updateGrant
+ * @description Servicio que se encarga de actualizar una beca
+ * @param {String} id - Id de la beca a actualizar
+ * @param {Object} grant - Objeto con los datos de la beca a actualizar 
+ * @returns 
+ */
 async function updateGrant(id, grant) {
     try {
         const grantFound = await Grant.findById(id);
@@ -70,19 +101,35 @@ async function updateGrant(id, grant) {
         handleError(error, "grant.service -> updateGrant");
     }
 }
-// NOTA: SUJETO A CAMBIOS, VERIFICAR SI UTILIZAR POR EL TEMA DE REGISTROS QUE TENGAN DATOS ASOCIADOS A LA BECA
+// <-------------------------------------------------------------------->
+/**
+ * @name deleteGrantById
+ * @description Servicio que se encarga de eliminar una beca
+ * @param {String} id - Id de la beca a eliminar 
+ * @returns 
+ */
 async function deleteGrantById(id) {
     try {
+        // Busqueda previa de la beca
         const grantFound = await Grant.findById(id);
+        // Verificacion de existencia de la beca
         if (!grantFound) return [null, "No se encontró la beca"];
+        // Eliminacion de beca
         const grantDeleted = await Grant.findByIdAndDelete(id);
+        // Retorno de la beca eliminada
         return [grantDeleted, null];
     } catch (error) {
         handleError(error, "grant.service -> deleteGrant");
     }
 }
 
-//  AGREGAR FUNCION PARA DESACTIVAR BECA (CAMBIAR ESTADO A FALSE)
+// <-------------------------------------------------------------------->
+/**
+ * @name desactivateGrantById
+ * @description Servicio que se encarga de desactivar una beca
+ * @param {String} id - Id de la beca a desactivar 
+ * @returns 
+ */
 async function desactivateGrantById(id) {
     try {
         const grantFound = await Grant.findById(id);
@@ -94,7 +141,12 @@ async function desactivateGrantById(id) {
     }
 }
 
-
+// <-------------------------------------------------------------------->
+/**
+ * @name checkGrant
+ * @description Servicio que se encarga de verificar si una beca ya expiró
+ * @returns 
+ */
 async function checkGrant() {
     try {
         const becas = await Grant.find({state: true});
@@ -105,6 +157,7 @@ async function checkGrant() {
         const diferenciaTiempo = Date.now() - fechaCreacion.getTime();
         const diasTranscurridos = diferenciaTiempo / (1000 * 3600 * 24); // Convertir a días
   
+        // Si han pasado 15 días desde la creación de la beca, se desactiva
         if (diasTranscurridos >= 15) {
           await desactivateGrantById(beca._id); // Desactivar la beca
         }
